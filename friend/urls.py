be8d7df31e5f488f0d160d22512bb2dc6002cfce
Newsujin <chemd78@naver.com>
2024-12-28 00:14:45 +0900

    :sparkles: feat: 수신한 친구 요청 리스트 API 엔드포인트 추가

--- friend/urls.py
+++ friend/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     SendFriendRequestView,
     RespondToFriendRequestView,
+	ReceivedFriendRequestListView,
     FriendListView,
     DeleteFriendView
 )
@@ -10,6 +11,7 @@ from .views import (
 urlpatterns = [
     path('request/', SendFriendRequestView.as_view(), name='request'),
     path('respond/', RespondToFriendRequestView.as_view(), name='respond'),
+	path('received-requests/', ReceivedFriendRequestListView.as_view(), name='received-requests'),
     path('list/', FriendListView.as_view(), name='list'),
     path('delete/', DeleteFriendView.as_view(), name='delete')
 ]
