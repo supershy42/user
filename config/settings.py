07570c7f5df96745d77f018c334ec50557138fd8
AhnJoonSung <tjdtna01@naver.com>
2024-12-29 00:46:22 +0900

    :green_heart: chore: localhost도 allow하게 추가

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-ALLOWED_HOSTS = ['api-gateway']
+ALLOWED_HOSTS = ['api-gateway', 'localhost']
 
 
 # Application definition
