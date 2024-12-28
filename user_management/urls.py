bc1ad80621cc9910aa251de25f0bc3d9ea1c7d6f
AhnJoonSung <tjdtna01@naver.com>
2024-12-28 16:35:54 +0900

    :sparkles: feat: 자신에 대한 접근과 다른 사람에 대한 접근을 엔드포인트 분리

--- user_management/urls.py
+++ user_management/urls.py
@@ -9,6 +9,7 @@ from .views import (
     UserProfileView,
     SendEmailView,
     VerifyCodeView,
+    MyProfileView,
 )
 
 urlpatterns = [
@@ -16,6 +17,7 @@ urlpatterns = [
     path('register/email-check/', EmailCheckAndSendCodeView.as_view(), name='email-check'),
     path('register/complete/', UserRegisterView.as_view(), name='register-complete'),
     path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/', MyProfileView.as_view(), name='my-profile'),
     path('profile/<int:user_id>/', UserProfileView.as_view(), name='user-profile'),
     path('send-email/', SendEmailView.as_view(), name='send-email'),
     path('2fa/', VerifyCodeView.as_view(), name='2fa')
